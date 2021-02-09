6f425054f5b7dcd32f8d28b5ce3b17a474d70174
ssong10 <leesy1403@naver.com>
2021-02-09 13:55:48 +0900

    chatting style

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,45 +1,72 @@
 const socket = io();
 const message = document.querySelector('.message')
 const chattings = document.querySelector('.chattings')
-const changeName = document.querySelector('.changeName')
+const nameInput = document.querySelector('.changeName')
+const sendBtn = document.querySelector('.send__btn')
+const ENTER_CODE = 13
+let prevChat = {}
 
-changeName.addEventListener('keydown',(e)=> {
-  let new_name = e.target.value;
-  socket.acks.name = new_name
+const checkInput = (input,inputValue) => {
+  if (!input.value) {
+    input.placeholder = `${inputValue}를 입력해주세요`
+    setTimeout(()=>{
+      input.placeholder = ''
+    },1000) 
+    return false
+  }
+  return true
+}
+const send = () => {
+  if (checkInput(nameInput,'이름') && checkInput(message,'메세지')){
+    const data = {id:socket.id,data:message.value,name:nameInput.value}
+    socket.emit('chat', data)
+    message.value = ''
+  }
+}
+
+const sentMessage = (data) => `
+  <div class="sent">
+    <span class="time">${data.time}</span>
+      <div class="chat">
+        <span>${data.data}</span>
+      </div>
+  </div>
+`
+const receviedMessage = (data,isEqual) => `
+  ${isEqual ? '' : `<span class="chat_name">${data.name}</span>`}
+  <div class="received">
+    <div class="chat">
+    <span>${data.data}</span>
+    </div>
+    <span class="time">${data.time}</span>
+  </div>
+`
+
+const render = (data) =>{
+  const equalUserTime = (prev,curr) => {
+    return prev.time === curr.time && prev.name === curr.name
+  }
+  const isEqual = equalUserTime(prevChat,data)
+  const chat = document.createElement('div')
+  chat.classList.add('chat-wrap')
+  if (isEqual){
+    const beforeTime = chattings.lastChild.querySelector('.time')
+    beforeTime.remove()
+  }
+  chat.innerHTML = data.id === socket.id ? sentMessage(data): receviedMessage(data,isEqual)
+  chattings.appendChild(chat)
+  prevChat = data
+  chattings.scrollTop = chattings.scrollHeight
+}
+
+socket.on('chat', (data)=>{
+  render(data)
 })
 
 message.addEventListener('keyup',(e)=>{
-  if (e.keyCode=== 13){
-    console.log(socket)
-    const data = {id:socket.id,data:e.target.value,name:socket.acks.name}
-    socket.emit('chat', data)
-    e.target.value = ''
+  if (e.keyCode=== ENTER_CODE){
+    send()
   }
 })
 
-const checkMsg = (id) => {
-  return id === socket.id
-}
-
-const render = (data) =>{
-  const chat = document.createElement('div')
-  chat.classList.add('chat-wrap')
-  const isMyMsg = checkMsg(data.id)
-  chat.innerHTML = isMyMsg ? 
-    `
-    <div class="chat sent">
-      <span>${data.data}</span>
-    </div>` 
-  :
-  `
-  <span class="chat_name">${data.name}</span>
-  <div class="chat received">
-    <span>${data.data}</span>
-  </div>
-  `
-  chattings.appendChild(chat)
-}
-socket.on('chat', (data)=>{
-    render(data)
-  }
-)
+sendBtn.addEventListener('click', send)
--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
 
+const time = () => {
+  const date = new Date()
+  const h = date.getHours()
+  const ap = h < 12 ? "오전" : "오후"
+  const hh = h < 12 ? (h+11)%12 + 1 : (h-1)%12+1
+  let mm = date.getMinutes()
+  mm = mm < 10 ? '0'+mm : mm
+  return `${ap} ${hh}:${mm}`
+}
+
 app.use(express.static(path.join(__dirname,'public')))
-let chattings = []
 io.on('connection', (socket) => {
-  console.log(`연결되었습니다`)
+  console.log(`연결되었습니다`, socket.id)
   socket.on('chat', data => {
-    io.emit('chat', data);
+    io.emit('chat', {...data,time:time() })
   });
 });
+
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
